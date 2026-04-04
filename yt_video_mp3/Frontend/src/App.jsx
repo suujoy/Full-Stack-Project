1026eb81a2c78a4e10d405df66860d8e92de28db
Sujoy Saha <ace.sujoy@gmail.com>
2026-04-04 21:32:03 +0530

    Delete yt_video_mp3 directory

--- yt_video_mp3/Frontend/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { RouterProvider } from "react-router";
-import { router } from "./features/app/app.route";
-
-const App = () => {
-    return <RouterProvider router={router} />;
-};
-
-export default App;
--- yt_video_mp3/Frontend/src/features/app/app.route.jsx
+++
@@ -1,9 +0,0 @@
-import { createBrowserRouter } from "react-router";
-import YtDownloadPage from "../ytDownload/pages/Downloadpage.jsx";
-
-export const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <YtDownloadPage />,
-    },
-]);
